c425eb8d0cb6d6bfd492d590c572c81da21b35e4
kingkoti <kingkoti@gmail.com>
2016-01-11 15:19:29 -0500

    Fully automated with crontab

--- modules/puppet/manifests/init.pp
+++ modules/puppet/manifests/init.pp
@@ -3,4 +3,23 @@ class puppet {
 		source => 'puppet:///modules/puppet/papply.sh',
 		mode => '0755',
 	}
+	
+	file { '/usr/local/bin/pull-updates':
+		source => 'puppet:///modules/puppet/pull-updates.sh',
+		mode => '0755',
+	}
+	
+	file { '/home/ec2-user/.ssh/id_rsa':
+		source => 'puppet:///modules/puppet/pup-cook-ec2.priv',
+		owner => 'ec2-user',
+		mode	=>	'0600',
+	}
+	
+	cron { 'run-puppet':
+		ensure	=>	'present',
+		user	=> 	'ec2-user',
+		command	=>	'/usr/local/bin/pull-updates',
+		minute	=> 	'*/10',
+		hour	=> 	'*',
+	}
 }
